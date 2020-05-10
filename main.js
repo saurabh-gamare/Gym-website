8a35329c87cdc3b10bcbce3acfce460a49b86af1
saurabh-gamare <59074172+saurabh-gamare@users.noreply.github.com>
2020-05-10 19:14:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function ham(){
+    // hamburger
+    var nav = document.querySelector('.nav-container');
+    nav.classList.toggle('nav-container-toggle')
+
+    // hamburger animation
+    var ani = document.querySelector('.ham');
+    ani.classList.toggle('ham-toggle')
+}
